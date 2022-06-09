3007eb5213ded366f5047bc29fa279ccf6a51634
Monaz Patel <monazpatel3399@gmail.com>
2022-06-09 19:15:50 +0530

    Loop

---
+++ LOOP/p48whileSer2.py
@@ -0,0 +1,5 @@
+n=int(input("Enter a Limit => "))
+i=1
+while i<=n:
+    print(i, i*i)
+    i=i+1
---
+++ LOOP/p49whileSer3.py
@@ -0,0 +1,5 @@
+n=int(input("Enter a Limit => "))
+i=1
+while i<=10:
+    print(n," x ",i," = ",i*n)
+    i=i+1
